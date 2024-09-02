0132bb14c78423ec54131468dbe35993da21465a
Hardik Chadha <134422569+hardik10-debug@users.noreply.github.com>
2024-09-02 20:32:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,27 +14,27 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Set up CORS to allow requests from your frontend (adjust the origin)
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+    origin: process.env.CLIENT_URL || 'https://front-e4ww.vercel.app/',
+    credentials: true
+};
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
-
-// api's
+// API routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-app.listen(PORT,()=>{
+// Start the server
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
-})
+});
